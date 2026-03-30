f8fa20232ab0d95d91c792a570cafaebba9a2175
AlexRotty5 <alexrott@stanford.edu>
2026-03-30 16:37:04 -0700

    cleanup before new project adds

--- src/pages/home/intro/index.tsx
+++ src/pages/home/intro/index.tsx
@@ -223,7 +223,7 @@ const IntroScreen = ({ isActive, activeSection, onBack, onComplete }: IntroScree
             onClick={handleArrowClick}
             className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-50 text-white/80 hover:text-white transition-colors duration-200 cursor-pointer"
           >
-            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <svg className="w-16 h-16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
             </svg>
           </motion.button>
